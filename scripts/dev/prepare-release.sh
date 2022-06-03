66b08fb8e96bf6698edf0f77993d20d5b3a2bb37
Robert Stoll <robert.stoll@tegonal.com>
2022-06-03 15:27:38 +0200

    don't update image in workflow so that we can use regular github token

--- scripts/dev/prepare-release.sh
+++ scripts/dev/prepare-release.sh
@@ -28,7 +28,4 @@ perl -0777 -i \
   -pe "s@(image: 'docker://tegonal/workflow-helper:)[^']+'@\${1}$version'@;" \
   ./action.yml
 
-find ./.github/workflows -name "*.yml" | xargs perl -0777 -i \
-  -pe "s#(uses: tegonal/workflow-helper@)#\${1}$version#;"
-
 git commit -a -m "$version"
